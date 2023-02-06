f3ac16cc987439827b7607307b52aa6365669be8
uniubesg <uniubesg@gmail.com>
2023-02-06 10:30:51 -0300

    Auste editor html

--- editar.php
+++ editar.php
@@ -38,14 +38,16 @@
         <form method="post">
             <div class="form-group">
                 <label for="numModulo">Número do Módulo</label>
-                <input type="text" class="form-control" id="numModulo" name="numModulo" value="<?php echo $dados["numero_modulo"]; ?>">
+                <input type="text" class="form-control" id="numModulo" name="numModulo" value="<?php echo $dados["numero_modulo"]; ?>" disabled>
             </div>
             <div class="form-group">
                 <label for="nomeModulo">Nome do Módulo</label>
                 <input type="text" class="form-control" id="nomeModulo" name="nomeModulo" value="<?php echo $dados["nome_modulo"]; ?>">
             </div>
-            <div class="form-group mt-2 mb-2">
-            <textarea id="div_editor1" name="conteudo" value="<?php echo $dados['conteudo_modulo']; ?>"></textarea>
+            <div class="form-group mt-2 mb-5">
+            <textarea id="div_editor1" name="conteudo">
+            <?php echo $dados['conteudo_modulo']; ?>
+            </textarea>
             <script>
                 var editor1 = new RichTextEditor("#div_editor1");
             </script>
